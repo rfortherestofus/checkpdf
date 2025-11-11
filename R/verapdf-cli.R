ffe0c7e74d28a1f84adcf95b90dfe774fd003fc6
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-11-11 19:31:07 +0100

    init basic

--- R/verapdf-cli.R
+++ R/verapdf-cli.R
@@ -4,16 +4,21 @@
 #' Utility to call verapdf command line interface.
 #'
 #' @param file PDF file to check.
+#' @param write_to Path to output JSON file. If `NULL`, not written.
 #'
 #' @returns output from the CLI
 #'
 #' @export
-verapdf <- function(file) {
+verapdf <- function(file, write_to = NULL) {
   cmd <- "verapdf"
   args <- c("--format", "json", file)
 
-  out <- system2(cmd, args, stdout = TRUE)
+  out <- suppressWarnings(system2(cmd, args, stdout = TRUE))
   json_out <- jsonlite::fromJSON(paste(out, collapse = "\n"))
+
+  if (!is.null(write_to)) {
+    jsonlite::write_json(json_out, write_to, auto_unbox = TRUE, pretty = TRUE)
+  }
 
   return(json_out)
 }
